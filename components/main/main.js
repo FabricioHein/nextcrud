7e54a48dcc89e6365c69831cceac9441829e9330
fabriciohein <fabricio.hein@live.com>
2021-09-23 18:05:41 -0300

    nextcru 1

---
+++ components/main/main.js
@@ -0,0 +1,27 @@
+import styles from './main.module.css'
+const Main = ()=>{
+
+return(   
+
+    <main className={styles.main}>
+    <h1 className={styles.title}>
+      Welcome to <a href="https://nextjs.org">Next.js!</a>
+    </h1>
+
+    <p className={styles.description}>
+      Get started by editing{' '}
+      <code className={styles.code}>pages/index.js</code>
+    </p>
+
+    <div className={styles.grid}>
+      <a href="/docs" className={styles.card}>
+        <h2>Documentation &rarr;</h2>
+        <p>Find in-depth information about Next.js features and API.</p>
+      </a>
+    </div>
+  </main>
+)
+
+}
+
+export default Main
